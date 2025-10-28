b32206066fc61510b7c0f383faaa950426b5b356
jamiedonnelly-px <jamie.donnelly@physicsx.ai>
2025-10-28 15:50:07 +0000

    change test.py

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-from gKDTree import cuNN
+from gKDTree import knn_search_3d
 import numpy as np
 from time import time
 from tqdm import tqdm
@@ -7,19 +7,18 @@ def test():
     N, q, k = 2**18, 2**22, 64
     points, queries = np.random.randn(N, 3), np.random.randn(q, 3)
     start = time()
-    _, gpu_indices = cuNN(points, queries, k, device="gpu")
+    _, gpu_indices = knn_search_3d(points, queries, k, backend="cuda")
     end = time()
     print(f"Took {end-start:.3f}s on GPU", flush=True)
 
     start = time()
-    _, cpu_indices = cuNN(points, queries, k, device="cpu")
+    _, cpu_indices = knn_search_3d(points, queries, k, backend="cpu")
     end = time()
     print(f"Took {end-start:.3f}s on CPU", flush=True)
 
     for i in tqdm(range(100)):
         assert np.all(sorted(cpu_indices[i]) == sorted(gpu_indices[i]))
     print(f"First {100} indices match perfectly.", flush=True)
-
 
 if __name__=="__main__":
     test()
